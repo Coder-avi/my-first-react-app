bd47f3454cd26faf71caecdf86ef307b30d75b83
Coder-avi <cbreaker942@gmail.com>
2021-08-11 11:37:47 +0200

    simple calc from reactjs import and export

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import {add , sub, mult, div} from './Calc';
 function App(){
     return(<>
     <ul>
-        <li>Sum of two number is {addEventListener(40, 4)}</li>
-        <li>Sub of two number is {addEventListener(40, 4)}</li>
-        <li>Mult of two number is {addEventListener(40, 4)}</li>
-        <li>Div of two number is {addEventListener(40, 4)}</li>
+        <li>Sum of two number is {add(40, 4)}</li>
+        <li>Sub of two number is {sub(40, 4)}</li>
+        <li>Mult of two number is {mult(40, 4)}</li>
+        <li>Div of two number is {div(40, 4)}</li>
        
     </ul>
     
--- src/Calc.jsx
+++ src/Calc.jsx
@@ -5,15 +5,15 @@ function add(a,b){
     return add;
 }
 function sub(a,b){
-    let sub = a +b;
+    let sub = a-b;
     return sub;
 }
 function mult(a,b){
-    let mult = a +b;
+    let mult = a*b;
     return mult;
 }
 function div(a,b){
-    let div = a +b;
+    let div = a/b;
     return div;
 }
 export {add , sub, mult, div};
